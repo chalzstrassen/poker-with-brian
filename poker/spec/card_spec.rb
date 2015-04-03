f58394769d5551e5a4c5acc6ec3ebe74f516be74
brian&charles <brian&charles@appacademy.io>
2015-04-03 17:52:33 -0400

    Test for numerical card value.

--- poker/spec/card_spec.rb
+++ poker/spec/card_spec.rb
@@ -1,4 +1,12 @@
 require 'card'
 
 describe Card do
+
+  describe "#numerical" do
+    it "should return the numerical value representing high card rank" do
+      seven_of_hearts = Card.new(:hearts, :seven)
+      expect(seven_of_hearts.numerical).to eq(6)
+    end
+  end
+
 end
